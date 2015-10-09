b1e3213abd36d3808fba511df7c0bcefa63a83b8
zlodiak <prozaik81-2@yandex.ru>
2015-10-09 10:54:18 +0300

    add nesting to cat

---
+++ db/migrate/20151009075248_add_nesting_to_category.rb
@@ -0,0 +1,7 @@
+class AddNestingToCategory < ActiveRecord::Migration
+  def change
+    add_column :categories, :parent_id, :integer
+    add_column :categories, :lft, :integer
+    add_column :categories, :rgt, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151009074615) do
+ActiveRecord::Schema.define(version: 20151009075248) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
     t.text     "desc"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "parent_id"
+    t.integer  "lft"
+    t.integer  "rgt"
   end
 
 end
